1267df6b442b0465c78baa48ff566f9a5f43cc4f
k1adi <rizkiadinugroho3@gmail.com>
2019-04-25 05:26:32 +0700

    minor changes

--- index.php
+++ index.php
@@ -22,14 +22,9 @@
 	<title>
 		Tukutiket
 	</title>
+
 </head>
 <body>
-	<?php
-		session_start();
-		if(isset($_SESSION['name'])){
-			$name = $_SESSION['name'];
-		}
-	?>
 
 	<!-- Navbar -->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top" id="navbar-home">
@@ -66,6 +61,7 @@
           		
           		<!-- Show Name After Login -->
           		<?php
+          		session_start();
           			if(isset($_SESSION['name'])){
 									$name = $_SESSION['name'];
 									echo $name;
@@ -101,7 +97,7 @@
 
 			<!-- Application -->
 			<div class="title mt-7 mb-4">
-				<i class="fas fa-ticket-alt"></i> Tukutiket
+				<i class="fas fa-ticket-alt" style="color:#ffc107;"></i> Tukutiket
 			</div>
 			<div class="h3 col-7 mx-auto">
 
@@ -109,45 +105,93 @@
 				Get your ticket and booking them Lorem Ipsum dolor sit amet consectetur adi piscing elit
 
 				<!-- Check your code booking -->
-				<div class="input-group mt-5">
+				<form action="/user/tiket.php" method="POST" class="input-group mt-5">
 				  <input type="text" class="form-control" placeholder="Check your code booking">
 				  <div class="input-group-append">
-				    <button class="btn btn-outline-primary" type="button">Search</button>
+				    <!-- <button class="btn btn-outline-primary" type="button">Search</button> -->
+				    <input type="submit" value="search" class="btn btn-outline-primary">
 				  </div>
-				</div>
+				</form>
 
 			</div>
 		</div>
 
-		<!-- Kereta Api-->
-		<div class="h4 text-center pt-6" id="kereta">
+		<!-- Cari Jadwal Kereta Api-->
+		<div class="h3 offset-md-4 pl-2 pt-6" id="kereta">
 			Kereta Api
 		</div>
 		<div class="row mb-5">
-			<div class="col-md-4 text-center">
-				<i class="fas fa-train my-3 title"></i>
+
+			<!-- Description for Train -->
+			<div class="col-md-4 text-center" style="height: 210px;">
+				<div class="image-kereta" title="Designed by macrovector / Freepik"></div>
 			</div>
 			<div class="col-md-8">
-				Veniam marfa mustache skateboard, adipisicing fugiat velit pitchfork beard. Freegan beard aliqua cupidatat mcsweeney's vero. Cupidatat four loko nisi, ea helvetica nulla carles. Tattooed cosby sweater food truck, mcsweeney's quis non freegan vinyl. Lo-fi wes anderson +1 sartorial. Carles non aesthetic exercitation quis gentrify. Brooklyn adipisicing craft beer vice keytar deserunt.
+				<p>Veniam marfa mustache skateboard, adipisicing fugiat velit pitchfork beard. Freegan beard aliqua cupidatat mcsweeney's vero. Cupidatat four loko nisi, ea helvetica nulla carles. Tattooed cosby sweater food truck, mcsweeney's quis non freegan vinyl. Lo-fi wes anderson +1 sartorial. Carles non aesthetic exercitation quis gentrify. Brooklyn adipisicing craft beer vice keytar deserunt.</p>
 			</div>
-			<div class="col-md-12">
-				
+			
+			<!-- Search Ticket for Trains -->
+			<div class="col-md-12 container mt-5">
+				<p class="mt-4 mb-3 h4">Cari Tiketmu !</p>
+				<form action="user/search_kereta.php" method="GET">
+					<div class="form-group row">
+						<div class="col">
+							<label>Stasiun Asal</label>
+							<input type="text" name="st_awal" class="form-control" required>
+						</div>
+						<div class="col">
+							<label>Stasiun Tujuan</label>
+							<input type="text" name="st_tujuan" class="form-control" required>
+						</div>
+						<div class="col">
+							<label>Tanggal Keberangkatan</label>
+							<input type="date" name="tgl_berangkat" class="form-control" required>
+						</div>
+						<div class="col">
+							<input type="submit" name="searchKA" value="search" class="btn btn-warning btn-block text-white" style="margin-top:2rem;">
+							<!-- <a href="user/search_kereta.php?tgl=$_POST['tgl_berangkat']&st_awal=$_POST['']" title=""></a> -->
+						</div>
+					</div>
+				</form>
 			</div>	
 		</div>
 
 		<!-- Pesawat -->
-		<div class="h4 text-center pt-6" id="pesawat">
+		<div class="h3 pt-6" id="pesawat">
 			Pesawat
 		</div>
 		<div class="row mb-5">
+
+			<!-- Description for Airplane -->
 			<div class="col-md-8">
-				Ad leggings keytar, brunch id art party dolor labore. Pitchfork yr enim lo-fi before they sold out qui. Tumblr farm-to-table bicycle rights whatever. Anim keffiyeh carles cardigan. Velit seitan mcsweeney's photo booth 3 wolf moon irure. Cosby sweater lomo jean shorts, williamsburg hoodie minim qui you probably haven't heard of them et cardigan trust fund culpa biodiesel wes anderson aesthetic. Nihil tattooed accusamus, cred irony biodiesel keffiyeh artisan ullamco consequat.
+				<p>Ad leggings keytar, brunch id art party dolor labore. Pitchfork yr enim lo-fi before they sold out qui. Tumblr farm-to-table bicycle rights whatever. Anim keffiyeh carles cardigan. Velit seitan mcsweeney's photo booth 3 wolf moon irure. Cosby sweater lomo jean shorts, williamsburg hoodie minim qui you probably haven't heard of them et cardigan trust fund culpa biodiesel wes anderson aesthetic. Nihil tattooed accusamus, cred irony biodiesel keffiyeh artisan ullamco consequat.</p>
 			</div>
-			<div class="col-md-4 text-center">
-				<i class="fas fa-plane"></i>
+			<div class="col-md-4 text-center" style="height: 210px;">
+				<div class="image-pesawat" title="Designed by macrovector / Freepik"></div>
 			</div>
-			<div class="col-md-12">
-				
+			
+			<!-- Search Ticket for Airplane -->
+			<div class="col-md-12 container mt-5">
+				<p class="mt-4 mb-3 h4">Cari Tiketmu !</p>
+				<form action="function/proccess_search_pesawat.php" method="POST">
+					<div class="form-group row">
+						<div class="col">
+							<label>Bandara Asal</label>
+							<input type="text" name="bandara_awal" class="form-control" required>
+						</div>
+						<div class="col">
+							<label>Bandara Tujuan</label>
+							<input type="text" name="bandara_tujuan" class="form-control" required>
+						</div>
+						<div class="col">
+							<label>Tanggal Keberangkatan</label>
+							<input type="date" name="tanggal_brgkt" class="form-control" required>
+						</div>
+						<div class="col">
+							<input type="submit" name="searchP" value="search" class="btn btn-warning btn-block text-white" style="margin-top:2rem;">
+						</div>
+					</div>
+				</form>
 			</div>	
 		</div>
 
@@ -178,10 +222,40 @@
 		<div class="h4 text-center pt-6" id="about">
 			About Us
 		</div>
-		<div class="row" style="height: 100vh;">
-			
+		<div class="row mb-4">
+					
 		</div>
 	</div>
 
+		<!-- Footer -->
+		<footer class="py-4 bg-dark" style="width:100%;">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="h3 text-white">
+							<i class="fas fa-ticket-alt"></i> Tukutiket
+						</div>
+					</div>
+					<div class="col">
+						<p class="text-white">Lorem</p>
+						<a href="#">Ipsum</a><br>
+						<a href="#">Ipsum</a><br>
+						<a href="#">Ipsum</a>
+					</div>
+					<div class="col">
+						<p class="text-white">Lorem</p>
+						<a href="#">Ipsum</a><br>
+						<a href="#">Ipsum</a><br>
+						<a href="#">Ipsum</a>
+					</div>
+					<div class="col">
+						<p class="text-white"> Social Media :</p>
+						<i class="fab fa-facebook fa-2x mr-3"></i>
+						<i class="fab fa-twitter fa-2x mr-3"></i>
+						<i class="fab fa-instagram fa-2x"></i>
+					</div>
+				</div>
+			</div>
+		</footer>
 </body>
 </html>
